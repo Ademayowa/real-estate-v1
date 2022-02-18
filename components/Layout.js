a59b90343df2e388801016e135a9e70eba3a1ca3
Ademayowa <ademayowaadeniyi@gmail.com>
2022-02-18 21:02:35 +0100

    added favicon

--- components/Layout.js
+++ components/Layout.js
@@ -6,8 +6,8 @@ export default function Layout({ title, description, keywords, children }) {
     <>
       <Head>
         <title>{title}</title>
-        <meta name='description' content='description' />
-        <meta name='keywords' content='keywords' />
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
         <link rel='icon' href='/favicon.png' />
       </Head>
 
@@ -20,6 +20,7 @@ export default function Layout({ title, description, keywords, children }) {
 }
 
 Layout.defaultProps = {
-  title: 'Real Estate Properties',
-  description: 'Find, Buy & rent houses in the UK',
+  title: 'Real Estate Properties in the UAE',
+  description: 'Find, Buy & rent houses in the UAE',
+  keywords: 'real estate, dubai, rent houses, buy houses',
 };
