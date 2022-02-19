b062a0afd7f77f850b67d1ce4b5d351a2f240248
Ademayowa <ademayowaadeniyi@gmail.com>
2022-02-19 10:16:39 +0100

    modified about property

--- pages/property/about.js
+++ pages/property/about.js
@@ -1,8 +1,10 @@
 export default function AboutPropertyPage({ description }) {
   return (
-    <div className='bg-white p-4 mt-4 shadow-sm'>
-      <h2 className='fs-6 fw-bold pt-3 pb-3'>About Property</h2>
-      <p className='text-secondary'>{description}</p>
+    <div className='bg-white mt-4 shadow-sm'>
+      <h2 className='fs-6 fw-bold pt-4 pb-4 border-bottom px-4'>
+        About Property
+      </h2>
+      <p className='text-secondary p-3 lh-lg'>{description}</p>
     </div>
   );
 }
